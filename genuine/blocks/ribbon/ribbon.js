0675bee2e04e34b499e3175fe3cc73b44d4e4103
hkhatana <hkhatana@adobe.com>
2025-12-30 13:06:19 +0530

    change ribbon functionality within a page

--- genuine/blocks/ribbon/ribbon.js
+++ genuine/blocks/ribbon/ribbon.js
@@ -1,30 +1,5 @@
-function createDiv(className) {
-  const div = document.createElement('div');
-  div.className = className;
-  return div;
-}
-
 export default function decorate(block) {
   if (!block) return;
-
-  const img = block.querySelector('img');
-  const link = block.querySelector('a[href]');
-  const textPara = block.querySelector('p:last-of-type');
-
-  if (!img || !link || !textPara) return;
-
-  img.alt = link.textContent.trim();
-
-  const logoWrapper = createDiv('ribbon__logo');
-  const titleWrapper = createDiv('ribbon__title');
-
-  const logoLink = document.createElement('a');
-  logoLink.className = 'ribbon__logo-link';
-  logoLink.href = link.href;
-
-  logoLink.append(img);
-  logoWrapper.append(logoLink);
-  titleWrapper.append(textPara);
-
-  block.replaceChildren(logoWrapper, titleWrapper);
+  const allParas = block.querySelectorAll('p');
+  block.replaceChildren(...allParas);
 }
