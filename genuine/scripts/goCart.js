e91e4279df22bcd0df478d9981056cc34d548d81
Drashti Modasara <dmodasara@Drashtis-MacBook-Pro.local>
2026-01-06 19:10:41 +0530

    config chnages

--- genuine/scripts/goCart.js
+++ genuine/scripts/goCart.js
@@ -2,6 +2,15 @@ import { getLibs, getConfig } from './utils.js';
 
 const miloLibs = getLibs('/libs');
 
+function getServiceURL(data, serviceName, envName) {
+  if (!data || !serviceName || !envName) return null;
+  const keyToFind = `${envName}.${serviceName}.url`;
+  const urlValue = data.find(item => item.key === keyToFind);
+  return urlValue ? urlValue.value : null;
+}
+
+
+
 const setApiKeyHeader = (opts, serviceName, envName) => {
   const keys = {
     gc: { stage: 'TargetingServiceInternal', prod: 'TargetingServiceInternal' },
@@ -14,16 +23,15 @@ const setApiKeyHeader = (opts, serviceName, envName) => {
 };
 
 export async function isTokenValid() {
-  const { default: getServiceConfig } = await import(
-    `${miloLibs}/utils/service-config.js`
-  );
-  const { codeRoot, env } = getConfig();
   const urlParams = new URLSearchParams(window.location.search);
   const gtoken = urlParams.get('gtoken');
   const gid = urlParams.get('gid');
   const serviceName = urlParams.get('serviceName') || 'gc';
-  const envName = urlParams.get('env') || env.name || 'prod'; // Priority: Query Param -> Milo Env -> Prod
-  const serviceConf = await getServiceConfig(codeRoot);
+  const envName = urlParams.get('env') || getConfig().env.name || 'prod'; // Priority: Query Param -> Milo Env -> Prod
+  const endptURL = '/genuine-shared/endpoints.json';
+  const res = await fetch(endptURL);
+  const response = await res.json();
+  const data = response.data;
 
   const formBody = Object.entries({ gid, gtoken })
     .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
@@ -38,7 +46,8 @@ export async function isTokenValid() {
 
     setApiKeyHeader(opts, serviceName, envName);
 
-    const response = await fetch(serviceConf[serviceName].url, opts);
+    const url = getServiceURL(data, serviceName, envName);
+    const response = await fetch(url, opts);
     return response.ok;
   } catch (err) {
     return false;
