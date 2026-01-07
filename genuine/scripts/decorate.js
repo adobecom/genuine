8eab899a54b24a538ce9d48699ce3876687d0af3
Nishant Kumar Thakur <nishant.thakur.ece@gmail.com>
2026-01-07 20:40:09 +0530

    MWPW-185837 Add disabled link support using "#_disabled" hash

--- genuine/scripts/decorate.js
+++ genuine/scripts/decorate.js
@@ -1,5 +1,23 @@
 import { getUrlParams } from './utils.js';
 
+const DISABLED_HASH = '#_disabled';
+
+function processDisabledLink(link) {
+  const href = link.getAttribute('href');
+  if (!href?.includes(DISABLED_HASH)) return false;
+
+  link.classList.add('link-disabled');
+  link.setAttribute('href', href.replace(DISABLED_HASH, ''));
+  link.setAttribute('aria-disabled', 'true');
+  link.setAttribute('tabindex', '-1');
+
+  const img = link.querySelector('img');
+  const ariaLabel = img?.alt ? `${img.alt}` : 'disabled link';
+
+  link.setAttribute('aria-label', ariaLabel);
+  return true;
+}
+
 function goCartLinkAppend(link, paramsValue) {
   try {
     const url = new URL(link.getAttribute('href'), window.location.origin);
@@ -18,19 +36,27 @@ function goCartLinkAppend(link, paramsValue) {
   }
 }
 
-export function decorateButton() {
+export function decorateLinks() {
   // Include links in header-localnav and main
   const links = document.querySelectorAll('header.local-nav a:not([href^="tel:"]), main a:not([href^="tel:"])');
   const cache = document.head.querySelector('meta[name="cache"]');
   const paramsValue = getUrlParams();
-  if (cache?.content === 'on' && Object.keys(paramsValue).length > 0) {
-    links.forEach((link) => goCartLinkAppend(link, paramsValue));
-  }
+  const shouldAppendParams = cache?.content === 'on' && Object.keys(paramsValue).length > 0;
+
+  links.forEach((link) => {
+    if (!processDisabledLink(link) && shouldAppendParams) {
+      goCartLinkAppend(link, paramsValue);
+    }
+  });
 
   const observer = new MutationObserver((_, obs) => {
     const localNav = document.querySelector('header.local-nav');
     if (!localNav) return;
-    document.querySelectorAll('header.local-nav a:not([href^="tel:"])').forEach((link) => goCartLinkAppend(link, paramsValue));
+    document.querySelectorAll('header.local-nav a:not([href^="tel:"])').forEach((link) => {
+      if (!processDisabledLink(link) && shouldAppendParams) {
+        goCartLinkAppend(link, paramsValue);
+      }
+    });
     obs.disconnect();
   });
 
