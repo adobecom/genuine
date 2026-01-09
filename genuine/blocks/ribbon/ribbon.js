e29264d6809377463e07723f4238d187326f0338
Nishant Thakur <nishant.thakur.ece@gmail.com>
2026-01-09 15:21:58 +0530

    MWPW-185771 Updated sizes and classes for Ribbon (#35)
    * MWPW-185771 Updated sizes and classesf for Ribbon
    
    * MWPW-185771: Removed debugger statement

--- genuine/blocks/ribbon/ribbon.js
+++ genuine/blocks/ribbon/ribbon.js
@@ -3,8 +3,8 @@ import { createTag } from '../../scripts/utils.js';
 // ===== CONFIGURATION =====
 
 const CONFIG = {
-  DEFAULT_SPACING: 'xxxs-spacing',
-  DEFAULT_LOGO_ICON: 'lg-logo-icon',
+  DEFAULT_SPACING: 'xxs-spacing',
+  DEFAULT_LOGO_ICON: 'l-logo-icon',
   DEFAULT_CLOSE_ICON: 'xxs-close-icon',
 };
 
